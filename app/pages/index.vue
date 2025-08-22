ae655e01a0e4e8e2c7bac2b153a7e3a3b2460aa1
Maprangsoft <maprangsoft@gmail.com>
2025-08-22 18:37:27 +0700

    move server to outside app

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 const { data } = await useFetch('/api/hello')
+console.log(`log => `, data.value);
 </script>
 
 <template>
-  <pre>{{ data }}</pre>
+  <!-- <pre>{{ data }}</pre> -->
 </template>
